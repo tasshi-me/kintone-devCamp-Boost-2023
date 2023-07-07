c2eff0e79caa0e3f644b418845442af61c7565f9
Masaharu TASHIRO <masatsr.kit@gmail.com>
2023-07-07 17:25:03 +0900

    add repo initialization process

---
+++ scripts/init-dotenv.mjs
@@ -0,0 +1,14 @@
+import path from "path";
+import { fileURLToPath } from "url";
+import { copyFile } from "fs/promises";
+import { existsSync } from "fs";
+
+const dirname = path.dirname(fileURLToPath(import.meta.url));
+const projectRoot = path.join(dirname, "../");
+
+const srcPath = path.join(projectRoot, ".env.template");
+const destPath = path.join(projectRoot, ".env");
+if (!existsSync(destPath)) {
+  console.log("initialize .env");
+  await copyFile(srcPath, destPath);
+}
---
+++ scripts/init-manifest-json.mjs
@@ -0,0 +1,14 @@
+import path from "path";
+import { fileURLToPath } from "url";
+import { copyFile } from "fs/promises";
+import { existsSync } from "fs";
+
+const dirname = path.dirname(fileURLToPath(import.meta.url));
+const projectRoot = path.join(dirname, "../");
+
+const srcPath = path.join(projectRoot, "customize-manifest.template.json");
+const destPath = path.join(projectRoot, "customize-manifest.json");
+if (!existsSync(destPath)) {
+  console.log("initialize customize-manifest.json");
+  await copyFile(srcPath, destPath);
+}

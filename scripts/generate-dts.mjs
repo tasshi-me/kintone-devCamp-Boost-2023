6b6c41054637ff205564e39fdca289ca1d159072
Masaharu TASHIRO <masatsr.kit@gmail.com>
2023-08-02 22:47:49 +0900

    update script to avoid WSL problem

--- scripts/generate-dts.mjs
+++ scripts/generate-dts.mjs
@@ -2,6 +2,7 @@ import path from "path";
 import { fileURLToPath } from "url";
 import { $ } from "zx";
 import minimist from "minimist";
+import { exec, execSync } from "child_process";
 
 const dirname = path.dirname(fileURLToPath(import.meta.url));
 const projectRoot = path.join(dirname, "../");
@@ -18,4 +19,10 @@ console.log(`Generating .d.ts file for App ${appId}...`);
 
 const destPath = path.join(projectRoot, "src", "@types", `app-${appId}.d.ts`);
 
-await $`kintone-dts-gen --app-id ${appId} --type-name Record --namespace App${appId} -o ${destPath}`;
+// On Windows environment, $`command` syntax will try to run command on WSL.
+// To avoid it, we use execSync instead.
+const stdout = await execSync(
+  `kintone-dts-gen --app-id ${appId} --type-name Record --namespace App${appId} -o ${destPath}`
+);
+console.log("" + stdout);
+// await $`kintone-dts-gen --app-id ${appId} --type-name Record --namespace App${appId} -o ${destPath}`;

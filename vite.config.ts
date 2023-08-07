67acba675c8a6c7f29f7fc88d3cbf0331f5e55fa
Masaharu TASHIRO <masatsr.kit@gmail.com>
2023-08-07 12:41:50 +0900

    add licenses to the bundled output

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,25 @@
 import { defineConfig } from "vite";
-import { resolve } from "path";
+import path, { resolve } from "path";
+import fs from "fs";
+import license from "rollup-plugin-license";
+
+const licenseText = fs.readFileSync(path.join(__dirname, "LICENSE"), "utf-8");
+const licenseTemplate = `
+Bundle of <%= pkg.name %>
+Generated: <%= moment().format('YYYY-MM-DD') %>
+Version: <%= pkg.version %>
+
+${licenseText}
+
+This bundle includes the following third-party libraries:
+<% _.forEach(dependencies, function (dependency) { %>
+  =====
+  <%= dependency.name %>@<%= dependency.version %> -- <%= dependency.license %>
+  -----
+
+  <%= dependency.licenseText %>
+<% }) %>
+`;
 
 export default defineConfig({
   build: {
@@ -7,6 +27,16 @@ export default defineConfig({
       name: "MyKintoneCustomize",
       entry: resolve(__dirname, "src/main.ts"),
       fileName: "my-kintone-customize",
+    },
+    rollupOptions: {
+      plugins: [
+        license({
+          banner: {
+            commentStyle: "ignored",
+            content: licenseTemplate,
+          },
+        }),
+      ],
     },
   },
 });
